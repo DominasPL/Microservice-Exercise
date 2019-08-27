51526f98f19e3cbd2208e49585b3e0c56bacebab
DomiasPL <dominik-stepuch@wp.pl>
2019-08-27 14:53:22 +0200

    Added User entity

---
+++ src/main/java/com/github/dominaspl/simplemicroservice/models/User.java
@@ -0,0 +1,22 @@
+package com.github.dominaspl.simplemicroservice.models;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@RequiredArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    
+}
